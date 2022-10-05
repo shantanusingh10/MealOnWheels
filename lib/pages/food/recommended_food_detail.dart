d3b073439048ca8f507f865128412a4a98eafb22
Shantanu Singh <shantanusingh994@gmail.com>
2022-10-05 22:50:18 +0530

    Completed recommended food detail pg ui

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:meal_on_wheels/widgets/app_icon.dart';
 import 'package:meal_on_wheels/widgets/big_text.dart';
 
+import '../../utils/colors.dart';
 import '../../utils/dimensions.dart';
 import '../../widgets/expandable_text_widget.dart';
 
@@ -62,6 +63,63 @@ class RecommendedFoodDetail extends StatelessWidget {
           )
         ]
       ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20*2.5,
+              right: Dimensions.width20*2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10
+            ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppIcon(iconSize: Dimensions.iconsize24,iconColor: Colors.white, backgroundColor: AppColors.mainColor,icon: Icons.remove),
+                  BigText(text: "Rs 140 " + " X " + " 0 ",color: AppColors.mainColor,size: Dimensions.font26,),
+                  AppIcon(iconSize: Dimensions.iconsize24,iconColor: Colors.white, backgroundColor: AppColors.mainColor,icon: Icons.add)
+
+                ],
+              )
+          ),
+          Container(
+            height: Dimensions.bottomHeightBar,
+            padding: EdgeInsets.only(top: Dimensions.height30,bottom: Dimensions.height30,left: Dimensions.width20,right: Dimensions.width20),
+            decoration: BoxDecoration(
+                color: Colors.white10,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20*2),
+                    topRight: Radius.circular(Dimensions.radius20*2)
+                )
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: Colors.white
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor
+                  )
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: Dimensions.height20,bottom: Dimensions.height20,left: Dimensions.width20,right: Dimensions.width20),
+                  child: BigText(text: "Rs 100 add to cart", color: Colors.white,),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: AppColors.mainColor
+                  ),
+                )
+              ],
+            ),
+          ),
+        ]
+      )
     );
   }
 }
